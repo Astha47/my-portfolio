160e1231c03dd4390f9a2db0f6a32a1700f4b256
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2024-01-28 22:50:02 +0700

    debug schema

--- src/app/lib/schema/Projects.js
+++ src/app/lib/schema/Projects.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose');
-const { title } = require('process');
+const { type } = require('os');
 
 const ProjectsSchema = new mongoose.Schema({
-    title: String,
-    status: String,
-    type: String,
-    imgurl: String,
-    desc: String,
-    repo: String,
-    url: String,
+    title:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true
+    },
+    type:{
+        type: String,
+        required: true
+    },
+    imgurl:{
+        type: String,
+    },
+    desc: {
+        type: String,
+    },
+    repo: {
+        type: String,
+    },
+    url: {
+        type: String,
+    },
 })
 
 const Projects = mongoose.models.Projects || mongoose.model("Projects", ProjectsSchema)
