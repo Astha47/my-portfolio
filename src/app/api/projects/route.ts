edf86fe569aa52494f3850a7f3ecf2445022d24b
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2024-01-28 07:20:56 +0700

    debuggingggg

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -2,6 +2,14 @@ import { dbConnect } from '../../lib/db'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const con = await dbConnect();
-  return NextResponse.json(con)
+  try {
+    await dbConnect();
+    return NextResponse.json({ message: 'Successfully connected to MongoDB' });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
+  }
 }
